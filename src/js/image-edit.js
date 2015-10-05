0bedccf0db0a73bc85bb98718fab83ec23991ec2
Souler <juanjo.herrero@upplication.com>
2015-10-05 13:41:15 +0200

    Add image edit. An image type with crop features

---
+++ src/js/image-edit.js
@@ -0,0 +1,39 @@
+(function() {
+    "use strict";
+
+    if (!SirTrevor)
+        return console.error("SirTrevor.Blocks.ImageEdit could not load because SirTrevor wasn't found");
+
+    SirTrevor.Blocks.ImageEdit = SirTrevor.Blocks.Image.extend({
+
+        type: "image-edit",
+
+        onDrop: function(transferData) {
+            var file = transferData.files[0],
+                urlAPI = (typeof URL !== "undefined") ? URL : (typeof webkitURL !== "undefined") ? webkitURL : null;
+
+            if (/image/.test(file.type)) {
+                this.$inputs.hide();
+                this.$editor.html($('<img>', { src: urlAPI.createObjectURL(file) })).show();
+                this.loading();
+                var $img = this.$editor.find('>img');
+                $img.cropper({
+                    built: (function () {
+                        // Expose the $cropper at block level
+                        this.$cropper = function() {
+                            return $img.cropper.apply($img, arguments);
+                        };
+                        this.ready();
+                    }).bind(this)
+                });
+            }
+        },
+
+        _serializeData: function() {
+            var data = { file: { url: null } };
+            if (this.$cropper)
+                data.file.url = this.$cropper('getCroppedCanvas').toDataURL()
+            return data;
+        },
+    });
+})()
