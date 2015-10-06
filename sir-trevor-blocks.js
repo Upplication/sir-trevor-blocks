dbe5e9093a889adcd052dbf9af6cd3ee6bc93d1c
Souler <juanjo.herrero@upplication.com>
2015-10-06 13:36:19 +0200

    bump package version

--- sir-trevor-blocks.js
+++ sir-trevor-blocks.js
@@ -297,6 +297,7 @@
     SirTrevor.Blocks.ImageEdit = SirTrevor.Blocks.Image.extend({
 
         type: "image_edit",
+        cropTimeout: 1000,
 
         onDrop: function(transferData) {
             var file = transferData.files[0],
@@ -306,15 +307,28 @@
                 this.$inputs.hide();
                 this.$editor.html($('<img>', { src: urlAPI.createObjectURL(file) })).show();
                 this.loading();
+                var timerCache = null;
                 var $img = this.$editor.find('>img');
                 $img.cropper({
-                    built: (function () {
+                    built: function () {
                         // Expose the $cropper at block level
                         this.$cropper = function() {
                             return $img.cropper.apply($img, arguments);
                         };
                         this.ready();
-                    }).bind(this)
+                    }.bind(this),
+                    crop: function(e) {
+                        // If we have a timer cache means user is cropping, cancel the previous timer
+                        if (timerCache)
+                            clearTimeout(timerCache);
+                        else
+                            SirTrevor.EventBus.trigger('image_edit:crop:start', [e]);
+
+                        timerCache = setTimeout(function () {
+                            timerCache = null;
+                            SirTrevor.EventBus.trigger('image_edit:crop:finish', [e]);
+                        }.bind(this), this.cropTimeout);
+                    }.bind(this)
                 });
             }
         },
