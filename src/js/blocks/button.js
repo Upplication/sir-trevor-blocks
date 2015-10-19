c8eecc4ebd32c1e77533ad4731699cea8d87293b
Souler <juanjo.herrero@upplication.com>
2015-10-19 11:57:02 +0200

    v2 Button working

--- src/js/blocks/button.js
+++ src/js/blocks/button.js
@@ -4,15 +4,6 @@
     if (!SirTrevor)
         return console.error("SirTrevor.Blocks.Button could not load because SirTrevor wasn't found");
 
-    var defaults = {
-        "background-color": "#00CA6B",
-        "width": "100%",
-        "line-height": "100%",
-        "border-color": "#4D4D4D",
-        "border-radius": "2px",
-        "border-width": "2px"
-    };
-
     SirTrevor.Blocks.Button = SirTrevor.Block.extend({
         type: 'button',
         title: function() { return i18n.t('blocks:button:title'); },
@@ -25,10 +16,7 @@
         onBlockRender: function() {
             // Setup shortcuts
             this.$preview = this.$el.find('.st-preview'); 
-            this.$css = this.$editor.find('[name^="css-"]');
-
-            // load the defaults
-            this._loadProperties();
+            this.$css = this.$editor.find('[name^="css-"]'); 
 
             // setup spectrum
             this.$editor.find('[type="color"]').spectrum({
@@ -38,7 +26,8 @@
             });
 
             // Set the default button text
-            this.$preview.find('[contenteditable="true"]').html(i18n.t("blocks:button:hint:text"));
+            if ($(this.getTextBlockHTML()).text().length <= 0)
+                this.setTextBlockHTML(i18n.t("blocks:button:hint:text"));
 
             // Listen for css inputs changes to refresh the preview
             this.$css.on('change input', this._onCssPropertyChange.bind(this));
@@ -47,38 +36,16 @@
             // Listen for the background-color changes to refresh the foreground color
             this.$css.filter('[name="css-background-color"]').on('change input', this._onBackgroundColorChange.bind(this))
             this.$css.filter('[name="css-background-color"]').on('move.spectrum', this._onBackgroundColorChange.bind(this));
+            // Refresh the preview
+            this.$css.trigger('change');
         },
 
         loadData: function(data) {
-            console.log("load data");
-            console.log(data);
-            this._loadedData = true;
-        },
-
-        _loadProperties: function() {
-            console.log("load default");
-            this.$css.each(function (i, el) {
-                var $el = $(el);
-                var property = $el.attr('name').replace(/^css\-/, '');
-
-                var rawValue = $el.val();
-                var units = $el.attr('units') || "";
-                var value = rawValue + units;
-
-                var defaultValue = defaults[property];
-                var defaultValueRaw = defaultValue.replace(units, '');
-
-                var currentValue = this._loadedData === true > 0 ? value : defaultValue;
-
-                console.log(property + " -----------------------")
-                console.log("rawValue" + rawValue);
-                console.log("defaultValue" + defaultValue);
-                console.log("value" + value);
-                console.log("currentValue" + currentValue);
-
-                this.$preview.css(property, currentValue);
-                $el.val(currentValue);
-            }.bind(this));
+            this.setTextBlockHTML(data.text);
+            Object.keys(data)
+            .forEach(function (key) {
+                this.$el.find('[name="'+ key + '"]').val(data[key]);
+            }.bind(this))
         },
 
         _onCssPropertyChange: function (ev, value) {
@@ -105,8 +72,6 @@
             var rgbPattern = /^rgb\(.*([0-9]+).*,.*([0-9]+),.*([0-9]+).*\)/;
 
             var color = $(c.target).spectrum('get').toHex();
-            console.log(c);
-            console.log(color);
 
             var r = parseInt(color.substr(0,2), 16);
             var g = parseInt(color.substr(2,2), 16);
