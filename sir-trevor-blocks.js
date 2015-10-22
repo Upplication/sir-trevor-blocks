ba1a9b35d354fc1e45ffc14faa453a21b932f65d
Souler <juanjo.herrero@upplication.com>
2015-10-22 11:55:47 +0200

    Button: foreground color. Change inherit to actual color

--- sir-trevor-blocks.js
+++ sir-trevor-blocks.js
@@ -193,7 +193,7 @@
             var b = parseInt(color.substr(4,2), 16);
 
             var yiq = ((r*299) + (g*587) + (b*114)) / 1000;
-            return (yiq >= 128) ? 'inherit' : '#FFFFFF';
+            return (yiq >= 128) ? '#000000' : '#FFFFFF';
         }
     })
 })();
