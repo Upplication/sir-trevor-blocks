8fb405ea95be2aac8293a3653875e7b77df49599
Souler <juanjo.herrero@upplication.com>
2015-10-28 10:13:17 +0100

    bump package version

--- sir-trevor-blocks.js
+++ sir-trevor-blocks.js
@@ -269,6 +269,7 @@
         validations: [ '_checkCropFinished' ],
 
         _isImageUploaded: function() {
+            var data = this._getData();
             if (data && data.file && data.file.url.length > 0)
                 return !/^data:image\/png;base64/.test(data.file.url);
             else
@@ -276,7 +277,6 @@
         },
 
         _checkCropFinished: function() {
-            var data = this._getData();
             if (!this._isImageUploaded())
                 this.setError(this.$inputs, i18n.t('blocks:image_edit:finish'));
         },
@@ -525,6 +525,7 @@
         }
     });
 })();
+spacer.js
 (function() {
     "use strict";
 
