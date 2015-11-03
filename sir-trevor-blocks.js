7af0b06db8da9aff91ea8a86495ab4ae81742b92
Souler <juanjo.herrero@upplication.com>
2015-11-03 12:29:44 +0100

    bump package version

--- sir-trevor-blocks.js
+++ sir-trevor-blocks.js
@@ -229,8 +229,8 @@
     if (!SirTrevor)
         return console.error("SirTrevor.Blocks.HeaderAlign could not load because SirTrevor wasn't found");
 
-    SirTrevor.Blocks.HeaderAlign = SirTrevor.Blocks.Header.extend({
-        type: 'header_align',
+    SirTrevor.Blocks.HeadingAlign = SirTrevor.Blocks.Heading.extend({
+        type: 'heading_align',
         alignable: true
     });
 })();
