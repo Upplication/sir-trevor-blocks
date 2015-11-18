12068c2403cadc0d7d9a20637cd7ab24c3a2578d
Souler <juanjo.herrero@upplication.com>
2015-11-18 14:28:07 +0100

    Widget: Add edit on data load.

--- src/js/blocks/widget.js
+++ src/js/blocks/widget.js
@@ -4,60 +4,71 @@
     if (!SirTrevor)
         return console.error("SirTrevor.Blocks.Widget could not load because SirTrevor wasn't found");
 
-    SirTrevor.Blocks.Widget = SirTrevor.Block.extend({
+    SirTrevor.Blocks.Widget = SirTrevor.Blocks.Text.extend({
 
-      type: "widget",
-      title: function() { return i18n.t('blocks:widget:title') },
-      icon_name: "code",
+        type: "widget",
+        title: function() { return i18n.t('blocks:widget:title') },
+        icon_name: "code",
+        editorHTML: '@@include("widget.html")',
 
-      pastable: true,
+        loadData: function(data) {
+            this.loadPastedContent(data.text);
+        },
 
-      paste_options: {
-        html: '@@include("widget.html")'
-      },
+        onBlockRender: function() {
+            var $textarea = this.$el.find('textarea');
+            var textarea = $textarea[0];
 
-      _serializeData: function() {
-        return {
-            format: 'html',
-            text: this.$el.find('textarea').val()
+            var offset = textarea.offsetHeight - textarea.clientHeight;
+         
+            $textarea.attr('placeholder', i18n.t('blocks:widget:hint'));
+            $textarea.on('keyup input', function() {
+                $textarea
+                    .css('height', 'auto')
+                    .css('height', textarea.scrollHeight + offset);
+            });
+        },
+
+        onContentPasted: function(ev) {
+            // If there is any content means the user is editing on the textarea itself,
+            // so dont trigger the paste event for preventing higlighting/formatting
+            if (this.$el.find('textarea').val().length <= 0)
+                this.loadPastedContent($(ev.target).val());
+        },
+
+        loadPastedContent: function(code) {
+            code = code || "";
+
+            var $find = this.$el.find.bind(this.$el);
+            var $preview = $find('.preview');
+            var $code = $preview.find('code');
+            var $editor = $find('.editor');
+            var $textarea = $find('textarea');
+            var $icon = $find('.st-icon');
+
+            // First load the code into the text area
+            $textarea.val(code);
+
+            // Replace < / > for its html chars
+            code = code.replace(/</g, '&lt;');
+            code = code.replace(/>/g, '&gt;');
+
+            // Add the code to the container and highlight it
+            $code.html(code);
+            hljs.highlightBlock($code[0]);
+
+            // Hide the editor
+            $icon.hide();
+            $editor.hide();
+            $preview.show();
+
+            // Add listener for editing the content
+            $preview.dblclick(function () {
+                $editor.show();
+                $preview.hide();
+                $textarea.trigger('input');
+            })
         }
-      },
-
-      isEmpty: function() {
-        return !(this.$el && this.$el.find('textarea').length > 0 && this.$el.find('textarea').val().length > 0);
-      },
-
-      loadData: function(data) {
-        this.loadPastedContent(data.text);
-      },
-
-      onContentPasted: function(ev) {
-        // If there is any content means the user is editing on the textarea itself,
-        // so dont trigger the paste event for preventing higlighting/formatting
-        if (this.$el.find('textarea').val().length <= 0)
-          this.loadPastedContent($(ev.target).val());
-      },
-
-      loadPastedContent: function(code) {
-        code = code || "";
-
-        // First load the code into the text area
-        this.$el.find('textarea').val(code);
-
-        // Replace < / > for its html chars
-        code = code.replace(/</g, '&lt;');
-        code = code.replace(/>/g, '&gt;');
-        // Prepare the containers
-        var preTag = $('<pre>');
-        var codeTag = $('<code>', { class: 'lang-html' });
-        // Add the code to the container and highlight it
-        codeTag.html(code);
-        hljs.highlightBlock(codeTag.get()[0]);
-        preTag.append(codeTag);
-        // Add the highlighted code to the editor and remove the input box
-        this.$editor.html(preTag);
-        this.$inputs.hide();
-      }
 
     });
 })();
