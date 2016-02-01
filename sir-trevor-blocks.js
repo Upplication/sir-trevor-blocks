d23882b442fd4d4337c548ca93862a4c33603237
Souler <soulerhyd@gmail.com>
2016-02-01 12:53:40 +0100

    bump package version

--- sir-trevor-blocks.js
+++ sir-trevor-blocks.js
@@ -278,13 +278,13 @@
                 extraPlugins: 'colorbutton,colordialog,font,justify',
                 // http://stackoverflow.com/questions/23538462/how-to-remove-buttons-from-ckeditor-4
                 toolbarGroups: [
-                    {"name":"basicstyles","groups":["basicstyles"]},
-                    {"name":"links","groups":["links"]},
-                    {"name":"paragraph","groups":["list","blocks", "align"]},
-                    {"name":"insert", "groups": ['Table']},
-                    {"name":"styles"},
-                    {"name":"colors"},
-                    {"name":"source", "groups": ['Source'] }
+                    {'name':'basicstyles','groups':['basicstyles']},
+                    {'name':'links','groups':['links']},
+                    {'name':'paragraph','groups': [ 'list', 'blocks', 'align' ]},
+                    {'name':'insert', 'groups': ['Table']},
+                    {'name':'styles'},
+                    {'name':'colors'},
+                    {'name':'document', 'groups': [ 'mode', 'document', 'doctools' ] },
                 ],
                 removeButtons: 'CreateDiv,Styles,Flash,Iframe,Image,HorizontalRule,Smiley,PageBreak,Anchor'
             });
