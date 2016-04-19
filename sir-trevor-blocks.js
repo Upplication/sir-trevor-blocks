3a4eb56045e4ca1d4c888980d664fb1b2ec8ffb1
Souler <soulerhyd@gmail.com>
2016-04-19 13:55:19 +0200

    bump package version

--- sir-trevor-blocks.js
+++ sir-trevor-blocks.js
@@ -47,6 +47,9 @@
                 },
                 ck_editor: {
                     title: "Text"
+                },
+                columns: {
+                    title: "Columns"
                 }
             }
         }
@@ -104,124 +107,15 @@
                 },
                 ck_editor: {
                     title: "Texto"
+                },
+                columns: {
+                    title: "Columnas"
                 }
             }
         }
     };
 
     jQuery.extend(true, SirTrevor.Locales, Locales);
-})();
-(function() {
-    var BlockMixins = SirTrevor.BlockMixins;
-    var Block = SirTrevor.Block;
-    Block.prototype.availableMixins.push('alignable');
-
-    var defaultAlignConfig = {
-        aligns : {
-            left: true,
-            center: true,
-            justify: true,
-            right: true
-        },
-        handler: function(align) {
-            this.getTextBlock().removeClass(function(index, classes) {
-                return classes.split(' ')
-                                .filter(function(a) { return /^st\-align-/.test(a) })
-                                .join(' ');
-            });
-            this.getTextBlock().addClass('st-align-' + align);
-        }
-    };
-
-    BlockMixins.Alignable = {
-        mixinName: 'Alignable',
-
-        initializeAlignable: function() {
-            // For this mixin to work we need to also have the controllable mixin available.
-            // Lets do some security checks
-            if ((this.controllable === true && !this.$control_ui) || // controllable is enabled here but not yet initialized
-                !this.controllable) { // we were not even marked as controllable
-                this.controls = this.controls || {};
-                this.withMixin(BlockMixins.Controllable);
-                this.controllable = false; // This will prevent from double initing the controllable ui
-            }
-
-            this.align_options = jQuery.extend(true, {}, defaultAlignConfig, this.align_options);
-            Object.keys(this.align_options.aligns).forEach(function (align) {
-                /*
-                 val might be:
-                    * `false`: Meaning this align should NOT be shown in the align controls.
-                    * `true`: Meaning this align will be shown on the align controls, but its behaviour will be handled by `align_options.handler` or the default `align_options.handler`.
-                    * `function() { ... } ` : A function that will be responsible for handling the click. It will be passed the align in css format (left, right, center, justify)
-                */
-                var val = this.align_options.aligns[align];
-
-                if (val == false) // Do not even show this on the control ui
-                    return;
-
-                var handler = this.align_options.handler;
-                if (val instanceof Function)
-                    handler = val;
-
-                if (!handler || !(handler instanceof Function))
-                    throw new Error('AlignableMixin: No valid handler function found for ' + align);
-
-                if (val === true) {
-                    this.addUiControl('align-' + align, function(e) {
-                        this._getData().align = align;
-                        (handler.bind(this))(align);
-                        if (e.originalEvent) { // Check if we were triggered by a human or not
-                            // If we were triggered by a human, raise the event, otherwise dont.
-                            // This allows us to use the handler click event for setting the initial
-                            // align in the block.
-                            console.log("raising block:aligned event");
-                            var ev = jQuery.Event();
-                            ev.originalEvent = e;
-                            ev.target = this.$el;
-                            ev.align = align;
-                            this.mediator.trigger('block:aligned', ev);
-                        }
-                    }.bind(this));
-                }
-            }.bind(this));
-
-            var data = this._getData();
-            if (data && data.align)
-                this.$control_ui.find('[class*="align-' + data.align + '"]').click();
-        },
-    }
-})();
-(function() {
-    var _ = window._;
-    var SirTrevor = window.SirTrevor;
-    var BlockMixins = SirTrevor.BlockMixins;
-    var Block = SirTrevor.Block;
-    Block.prototype.availableMixins.push('clonable');
-
-    BlockMixins.Clonable = {
-        mixinName: 'Clonable',
-
-        initializeClonable: function() {
-            // For this mixin to work we need to also have the controllable mixin available.
-            // Lets do some security checks
-            if ((this.controllable === true && !this.$control_ui) || // controllable is enabled here but not yet initialized
-                !this.controllable) { // we were not even marked as controllable
-                this.controls = this.controls || {};
-                this.withMixin(BlockMixins.Controllable);
-                this.controllable = false; // This will prevent from double initing the controllable ui
-            }
-
-            var stInstance = SirTrevor.getInstance(this.instanceID);
-
-            this.addUiControl('clone', function(e) {
-                var blockData = _.cloneDeep(this.getData());
-                var blocks = stInstance.block_manager.blocks;
-                stInstance.block_controls.currentContainer = this.$el; // This will determine where the new block is added
-                stInstance.block_manager.createBlock(blockData.type, blockData.data);
-                SirTrevor.EventBus.trigger('block:cloned', blocks[blocks.length - 1]);
-            }.bind(this))
-        },
-    }
 })();
 (function() {
     "use strict";
@@ -461,6 +355,70 @@
                 text: this.ckeditor ? this.ckeditor.getData() : ''
             }
         }
+    });
+})();
+(function() {
+    "use strict";
+
+    if (!SirTrevor)
+        return console.error("SirTrevor.Blocks.Columns could not load because SirTrevor wasn't found");
+
+    SirTrevor.Blocks.Columns = SirTrevor.Block.extend({
+
+      type: "columns",
+      title: function() { return i18n.t('blocks:columns:title') },
+
+      _editorsSelectors: [
+      	'.st-columns-editor-left > .editor',
+      	'.st-columns-editor-right > .editor'
+      ],
+
+      _editors: [],
+
+      editorHTML: '<div class="st-columns-block"><div class="st-columns-editor st-columns-editor-left"><textarea class="editor"></textarea></div><div class="st-columns-editor st-columns-editor-right"><textarea class="editor"></textarea></div></div>',
+
+      onBlockRender: function() {
+      	console.log('onBlockRender: Passed!')
+      	var self = this;
+      	var parentStEditor = SirTrevor.getInstance(this.instanceID);
+      	var blockTypes = parentStEditor.options.blockTypes.filter(function(type) {
+      		return type.toLowerCase() != self.type;
+      	});
+
+      	self._editorsSelectors.forEach(function(selector, idx) {
+      		self._editors[idx] = new SirTrevor.Editor({
+      			el: self.$(selector),
+      			blockTypes: blockTypes,
+      			blockLimit: 1,
+      		});
+      	})
+      },
+
+      loadData: function(data) {
+      	var self = this;
+      	data.columns.forEach(function(column, idx) {
+      		var selector = self._editorsSelectors[idx];
+      		self.$(selector).val(JSON.stringify( { data: column.blocks } ));
+      	})
+      },
+
+      _serializeData: function() {
+      	var self = this;
+        return {
+        	columns: self._editors.map(function(st) {
+        		return {
+        			blocks: self._retrieveEditorData(st)
+        		}
+        	})
+        };
+      },
+
+      _retrieveEditorData: function(editor) {
+        // Force SirTrevor to update its internal data store
+        editor.store.reset();
+        editor.validateBlocks(false);
+        return editor.store.retrieve().data;
+      }
     });
 })();
 (function() {
@@ -990,4 +948,116 @@
         }
 
     });
+})();
+(function() {
+    var BlockMixins = SirTrevor.BlockMixins;
+    var Block = SirTrevor.Block;
+    Block.prototype.availableMixins.push('alignable');
+
+    var defaultAlignConfig = {
+        aligns : {
+            left: true,
+            center: true,
+            justify: true,
+            right: true
+        },
+        handler: function(align) {
+            this.getTextBlock().removeClass(function(index, classes) {
+                return classes.split(' ')
+                                .filter(function(a) { return /^st\-align-/.test(a) })
+                                .join(' ');
+            });
+            this.getTextBlock().addClass('st-align-' + align);
+        }
+    };
+
+    BlockMixins.Alignable = {
+        mixinName: 'Alignable',
+
+        initializeAlignable: function() {
+            // For this mixin to work we need to also have the controllable mixin available.
+            // Lets do some security checks
+            if ((this.controllable === true && !this.$control_ui) || // controllable is enabled here but not yet initialized
+                !this.controllable) { // we were not even marked as controllable
+                this.controls = this.controls || {};
+                this.withMixin(BlockMixins.Controllable);
+                this.controllable = false; // This will prevent from double initing the controllable ui
+            }
+
+            this.align_options = jQuery.extend(true, {}, defaultAlignConfig, this.align_options);
+            Object.keys(this.align_options.aligns).forEach(function (align) {
+                /*
+                 val might be:
+                    * `false`: Meaning this align should NOT be shown in the align controls.
+                    * `true`: Meaning this align will be shown on the align controls, but its behaviour will be handled by `align_options.handler` or the default `align_options.handler`.
+                    * `function() { ... } ` : A function that will be responsible for handling the click. It will be passed the align in css format (left, right, center, justify)
+                */
+                var val = this.align_options.aligns[align];
+
+                if (val == false) // Do not even show this on the control ui
+                    return;
+
+                var handler = this.align_options.handler;
+                if (val instanceof Function)
+                    handler = val;
+
+                if (!handler || !(handler instanceof Function))
+                    throw new Error('AlignableMixin: No valid handler function found for ' + align);
+
+                if (val === true) {
+                    this.addUiControl('align-' + align, function(e) {
+                        this._getData().align = align;
+                        (handler.bind(this))(align);
+                        if (e.originalEvent) { // Check if we were triggered by a human or not
+                            // If we were triggered by a human, raise the event, otherwise dont.
+                            // This allows us to use the handler click event for setting the initial
+                            // align in the block.
+                            console.log("raising block:aligned event");
+                            var ev = jQuery.Event();
+                            ev.originalEvent = e;
+                            ev.target = this.$el;
+                            ev.align = align;
+                            this.mediator.trigger('block:aligned', ev);
+                        }
+                    }.bind(this));
+                }
+            }.bind(this));
+
+            var data = this._getData();
+            if (data && data.align)
+                this.$control_ui.find('[class*="align-' + data.align + '"]').click();
+        },
+    }
+})();
+(function() {
+    var _ = window._;
+    var SirTrevor = window.SirTrevor;
+    var BlockMixins = SirTrevor.BlockMixins;
+    var Block = SirTrevor.Block;
+    Block.prototype.availableMixins.push('clonable');
+
+    BlockMixins.Clonable = {
+        mixinName: 'Clonable',
+
+        initializeClonable: function() {
+            // For this mixin to work we need to also have the controllable mixin available.
+            // Lets do some security checks
+            if ((this.controllable === true && !this.$control_ui) || // controllable is enabled here but not yet initialized
+                !this.controllable) { // we were not even marked as controllable
+                this.controls = this.controls || {};
+                this.withMixin(BlockMixins.Controllable);
+                this.controllable = false; // This will prevent from double initing the controllable ui
+            }
+
+            var stInstance = SirTrevor.getInstance(this.instanceID);
+
+            this.addUiControl('clone', function(e) {
+                var blockData = _.cloneDeep(this.getData());
+                var blocks = stInstance.block_manager.blocks;
+                stInstance.block_controls.currentContainer = this.$el; // This will determine where the new block is added
+                stInstance.block_manager.createBlock(blockData.type, blockData.data);
+                SirTrevor.EventBus.trigger('block:cloned', blocks[blocks.length - 1]);
+            }.bind(this))
+        },
+    }
 })();
