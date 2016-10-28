f05fbae2c432ca7214258c985ce7f4ded0fba0e6
Juan José <juanjo.herrero@upplication.com>
2016-10-28 11:50:51 +0200

    bump package version

--- sir-trevor-blocks.js
+++ sir-trevor-blocks.js
@@ -53,13 +53,13 @@
 	Utils.loadBlock(__webpack_require__(26));
 	Utils.loadBlock(__webpack_require__(30));
 	Utils.loadBlock(__webpack_require__(33));
+	Utils.loadBlock(__webpack_require__(35));
 	Utils.loadBlock(__webpack_require__(36));
 	Utils.loadBlock(__webpack_require__(37));
-	Utils.loadBlock(__webpack_require__(38));
-	Utils.loadBlock(__webpack_require__(40));
+	Utils.loadBlock(__webpack_require__(39));
 
+	Utils.loadMixin(__webpack_require__(42));
 	Utils.loadMixin(__webpack_require__(43));
-	Utils.loadMixin(__webpack_require__(44));
 
 
 /***/ },
@@ -354,7 +354,13 @@
 	        });
 
 	        // Set the default button text
-	        if ($(this.getTextBlockHTML()).text().length <= 0)
+	        var content = this.getTextBlockHTML();
+
+	        try { // Sometimes there will be a enclosing <p>, sometimes not :/
+	            content = $(content).text()
+	        } catch (e) {} // Do nothing
+
+	        if (content.length <= 0)
 	            this.setTextBlockHTML(i18n.t("blocks:button:hint:text"));
 
 	        this.$editor.find('[name="user-href"]')
@@ -592,7 +598,7 @@
 	var i18n = __webpack_require__(28);
 	var SirTrevor = __webpack_require__(23);
 	var editorHTML = __webpack_require__(34);
-	var positionerFix = __webpack_require__(35);
+	var positionerFix = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./positioner-fix.html\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 
 	module.exports = SirTrevor.Block.extend({
 
@@ -690,12 +696,6 @@
 
 /***/ },
 /* 35 */
-/***/ function(module, exports) {
-
-	module.exports = "<style id=\"st-block-positioner-fix\">.st-block-positioner { display: none; }</style>";
-
-/***/ },
-/* 36 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var _ = __webpack_require__(27);
@@ -935,7 +935,7 @@
 	});
 
 /***/ },
-/* 37 */
+/* 36 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var _ = __webpack_require__(27);
@@ -1085,13 +1085,13 @@
 	});
 
 /***/ },
-/* 38 */
+/* 37 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var _ = __webpack_require__(27);
 	var i18n = __webpack_require__(28);
 	var SirTrevor = __webpack_require__(23);
-	var editorHTML = __webpack_require__(39);
+	var editorHTML = __webpack_require__(38);
 
 	module.exports = SirTrevor.Block.extend({
 
@@ -1126,20 +1126,20 @@
 	});
 
 /***/ },
-/* 39 */
+/* 38 */
 /***/ function(module, exports) {
 
 	module.exports = "<div class=\"st-control\">\n    <div class=\"st-value-container\">\n    \t<span><%= i18n.t(\"blocks:spacer:size\") %></span>\n        <input class=\"st-value\" name=\"height\" type=\"range\" value=\"5\" units=\"vw\" step=\"0.1\" max=\"50\" min=\"0\" />\n    </div>\n    <span class=\"st-output\"></span>\n</div>";
 
 /***/ },
-/* 40 */
+/* 39 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var $ = __webpack_require__(22);
 	var i18n = __webpack_require__(28);
-	var hljs = __webpack_require__(41)
+	var hljs = __webpack_require__(40)
 	var SirTrevor = __webpack_require__(23);
-	var editorHTML = __webpack_require__(42);
+	var editorHTML = __webpack_require__(41);
 
 	module.exports = SirTrevor.Blocks.Text.extend({
 
@@ -1224,19 +1224,19 @@
 	});
 
 /***/ },
-/* 41 */
+/* 40 */
 /***/ function(module, exports) {
 
 	module.exports = hljs;
 
 /***/ },
-/* 42 */
+/* 41 */
 /***/ function(module, exports) {
 
 	module.exports = "<div class=\"st-widget-editor-container\">\n    <div class=\"editor\">\n        <span class=\"st-icon\"></span>\n        <textarea name=\"text\"></textarea>\n    </div>\n    <div style=\"display: none\" class=\"preview\">\n        <pre><code class=\"lang-html\"></code></pre>\n    </div>\n</div>";
 
 /***/ },
-/* 43 */
+/* 42 */
 /***/ function(module, exports) {
 
 	var defaultAlignConfig = {
@@ -1315,7 +1315,7 @@
 	};
 
 /***/ },
-/* 44 */
+/* 43 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var _ = __webpack_require__(27);
